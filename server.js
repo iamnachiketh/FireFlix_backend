ff8de7ff519448e4ca9a0773c1569ccee9c1cd9c
Nachiketha <nachiketh.k.c@brainsightai.com>
2023-07-19 21:46:02 +0530

    port

--- server.js
+++ server.js
@@ -202,7 +202,7 @@ app.put('/user/deleteaccount',(req,res)=>{
         }
     });
 })
-
-app.listen(3001,"0.0.0.0",()=>{
-    console.log('listening !!!');
+const port = process.env.PORT || 3001
+app.listen(port,"0.0.0.0",()=>{
+    console.log('listening !!!',port);
 });
